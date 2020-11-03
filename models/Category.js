5cee198a89f6141a913675ba48f7e9d7fbf45a1f
jespi116 <jespi116@fiu.edu>
2020-11-03 01:49:39 -0500

    added model columns, working on routes

--- models/Category.js
+++ models/Category.js
@@ -6,9 +6,29 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
